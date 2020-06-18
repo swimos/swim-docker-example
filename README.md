21208fb3d48d992eca828e962bab7fb6a2e324ca
SirCipher <tklapwijk92@gmail.com>
2020-06-18 08:29:39 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# swim-docker-example
+
+This is a small example application demonstrating how to build a Java project into a Docker image using the [Gradle docker plugin](https://github.com/bmuschko/gradle-docker-plugin).
+
+The application itself is composed of two parts: 
+- A basic plane that stores some arbitrary information using a Value Lane and contains a Command Lane for updating it.
+- A client application that is used for reading and writing to the plane.
+
+The Docker image exposes port 9001 as this is what the server is configured to use in the `resources/server.recon` configuration file.
+
+This project can be built into a Docker image as follows:
+```
+./gradlew dockerBuildImage
+
+$ docker image ls
+REPOSITORY                       TAG                 IMAGE ID            CREATED             SIZE
+org.swimos/swim-docker-example   1.1                 17d69579cdf8        1 second ago        342MB
+```
+
---
+++ src/main/java/basic/UnitAgent.java
@@ -0,0 +1,37 @@
+// Copyright 2015-2019 SWIM.AI inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package basic;
+
+import swim.api.SwimLane;
+import swim.api.agent.AbstractAgent;
+import swim.api.lane.CommandLane;
+import swim.api.lane.ValueLane;
+import java.time.ZonedDateTime;
+
+public class UnitAgent extends AbstractAgent {
+
+  @SwimLane("info")
+  ValueLane<String> info = this.<String>valueLane()
+      .didSet((newValue, oldValue) -> logMessage(ZonedDateTime.now() + ": `info` set to {" + newValue + "} from {" + oldValue + "}"));
+
+  @SwimLane("publishInfo")
+  CommandLane<String> publishInfo = this.<String>commandLane()
+      .onCommand(msg -> this.info.set("from publishInfo: " + msg));
+
+  private void logMessage(Object msg) {
+    System.out.println(nodeUri() + ": " + msg);
+  }
+
+}
